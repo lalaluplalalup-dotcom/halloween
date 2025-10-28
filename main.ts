920d412bcbcc6fcbbf15f730a993500abfbcccbf
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2025-10-28 08:40:55 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,48 +1,30 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    record.startRecording(record.BlockingState.Blocking)
-})
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    record.playAudio(record.BlockingState.Blocking)
-})
-record.setMicGain(record.AudioLevels.Low)
 basic.forever(function () {
-    if (input.lightLevel() > 50) {
-        for (let Index = 0; Index <= 2; Index++) {
-            led.plot(Index, 2)
-            led.plot(Index + 2, 2)
-            basic.pause(500)
-            led.unplot(Index, 2)
-            led.unplot(Index + 2, 2)
-        }
-        record.playAudio(record.BlockingState.Blocking)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . #
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # . # .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # . # . .
-            . . . . .
-            . . . . .
-            `)
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+    for (let Index = 0; Index <= 2; Index++) {
+        led.plot(Index, 2)
+        led.plot(Index + 2, 2)
+        basic.pause(500)
+        led.unplot(Index, 2)
+        led.unplot(Index + 2, 2)
     }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . .
+        . . . . .
+        . . . . .
+        `)
 })
